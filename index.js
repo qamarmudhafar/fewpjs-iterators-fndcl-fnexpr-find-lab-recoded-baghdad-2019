73da16d8d9b58fdf38486b89f2bf094d9205a024
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-15 06:48:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const testVar = {}
+let record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
 
-function testFunc() {
-  return "hi"
+let superbowlWin=function(arr){
+  let find=arr.find(function(w){
+    return w.result==="W";
+  })
+  if(find===undefined){
+    return "NO WINS"
+  }
+  else {
+    return find.year;
+  }
 }
+
+superbowlWin(record)
